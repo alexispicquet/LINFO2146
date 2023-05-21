d60c98377644f401d7783f60db3cb5f02816f1c5
brpierre <62648586+brpierre@users.noreply.github.com>
2023-05-21 18:02:45 +0200

    Update coordinator.c

--- coordinator.c
+++ coordinator.c
@@ -19,59 +19,21 @@
 /* Configuration */
 #define SEND_INTERVAL (4 * CLOCK_SECOND)
 
-// Sync interval for Berkeley Time Synchronization Algorithm
-#define SYNC_INTERVAL 10
-
 #if MAC_CONF_WITH_TSCH
 #include "net/mac/tsch/tsch.h"
 //static linkaddr_t dest_addr =         {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
 static linkaddr_t coordinator_addr =  {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
 #endif /* MAC_CONF_WITH_TSCH */
 
+
+//static linkaddr_t parent;
 static pkt_list_t* neighbours;
 static pkt_list_t* to_ack;
 static pkt_list_t* kids;
 
-// Variable for storing local time offset
-static clock_time_t time_offset = 0;
-
 /*---------------------------------------------------------------------------*/
 PROCESS(nullnet_example_process, "NullNet broadcast example");
 AUTOSTART_PROCESSES(&nullnet_example_process);
-
-/*---------------------------------------------------------------------------*/
-void adjust_local_clock(clock_time_t adjustment) {
-    time_offset += adjustment;
-}
-
-// Berkeley Time Synchronization Algorithm
-void berkeley_sync_algorithm() {
-    int node_count = size_list(neighbours);
-    clock_time_t time_sum = 0;
-
-    // Sum all clock values from neighbours
-    pkt_list_t* temp = neighbours;
-    while (temp != NULL) {
-        time_sum += temp->head->timestamp;
-        temp = temp->next;
-    }
-
-    // Average time
-    clock_time_t average_time = time_sum / node_count;
-
-    // Adjust local clock
-    adjust_local_clock(average_time - clock_time());
-
-    // Update all neighbours
-    temp = neighbours;
-    while (temp != NULL) {
-        clock_time_t adjustment = average_time - temp->head->timestamp;
-        // Send adjustment to neighbour. This is a placeholder function
-        // and you would need to implement it according to your specific needs
-        send_time_adjustment(&temp->src, adjustment);
-        temp = temp->next;
-    }
-}
 
 /*---------------------------------------------------------------------------*/
 void input_callback(const void *data, uint16_t len,
@@ -97,7 +59,6 @@ void input_callback(const void *data, uint16_t len,
     LOG_INFO_("\n");
   }
 }
-
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(nullnet_example_process, ev, data)
 {
@@ -122,30 +83,32 @@ PROCESS_THREAD(nullnet_example_process, ev, data)
   etimer_set(&periodic_timer, 4* CLOCK_SECOND);
   
   while(1) {
-    //TODO lower if routing must be always perfect => overhead
+    //TODO lower if routing must be always perfect
     if(iter>20) {iter=0;}
     iter++;
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-    if(iter % SYNC_INTERVAL == 0) {
-    berkeley_sync_algorithm();
-    }
     // broadcast if no neighbours or periodically
-    clock_time_t current_time = clock_time() + time_offset; // Get synchronized time
-    if (current_time % (TOTAL_TIMESLOTS * TIMESLOT_DURATION) == MY_TIMESLOT * TIMESLOT_DURATION) {
-    // It's my timeslot, do the transmission
-    nullnet_buf = (uint8_t *)pkt;
-    nullnet_len = sizeof(*pkt);
-    memcpy(nullnet_buf, pkt, sizeof(*pkt));
-    nullnet_len = sizeof(*pkt);
-    NETSTACK_NETWORK.output(NULL);
-    action = TRUE;
-    } else {
-    // It's not my timeslot, wait for the next timeslot
-    etimer_set(&periodic_timer, (MY_TIMESLOT * TIMESLOT_DURATION) - (current_time % (TOTAL_TIMESLOTS * TIMESLOT_DURATION)));
-    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+    if(iter==0 || size_list(neighbours)==0) {
+        nullnet_buf = (uint8_t *)pkt;
+        nullnet_len = sizeof(*pkt);
+        pkt = init_pkt(SEARCH, COOR, 0);
+        LOG_INFO_("COOR %u broadcast",node_id);
+        LOG_INFO_("\n");
+    
+        memcpy(nullnet_buf, pkt, sizeof(*pkt));
+        nullnet_len = sizeof(*pkt);
+
+        NETSTACK_NETWORK.output(NULL);
+        action = TRUE;
     }
     // update kids counter (+1)
     pkt_list_t* temp = kids;
+    while(temp!=NULL && temp->head!=NULL) {
+        temp->counter += 1;
+        temp = temp->next;
+    }
+    // update neighbours counter (+1)
+    temp = neighbours;
     while(temp!=NULL && temp->head!=NULL) {
         temp->counter += 1;
         temp = temp->next;
@@ -200,6 +163,24 @@ PROCESS_THREAD(nullnet_example_process, ev, data)
             temp = temp->next;
         }
     }
+    /*/ remove neighbours
+    temp = neighbours;
+    while(temp!=NULL && temp->head!=NULL) {
+        if(temp->counter > 10) {
+            pkt_list_t* first;
+            free_pkt(temp->head);
+            first = temp;
+            temp = temp->next;
+        
+            LOG_INFO_("remove neighbour ");
+            LOG_INFO_LLADDR(&first->src);
+            LOG_INFO_("\n");
+            
+            free(first);
+        } else {
+            temp = temp->next;
+        }
+    }*/
     // free messages received
     free_list(to_ack);
     to_ack = NULL;
@@ -214,11 +195,6 @@ PROCESS_THREAD(nullnet_example_process, ev, data)
     nullnet_buf = NULL;
     nullnet_len = 0;
     etimer_reset(&periodic_timer);
-
-    // Call Berkeley Time Synchronization Algorithm every SYNC_INTERVAL iteration
-    if(iter % SYNC_INTERVAL == 0) {
-        berkeley_sync_algorithm();
-    }
   }
   free_pkt(pkt);
   free_list(neighbours);
